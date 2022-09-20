f48c411f2db02e2a1e3ccc1447ea12e391ac1b9c
diegojojoayandun <xcryptorlabs@gmail.com>
2022-09-20 11:16:01 -0500

    update source, add number total of repos h2 tag

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -1,33 +1,38 @@
 import { useState, useEffect } from "react";
-import getReactRepositories from "../helpers/getItem";
+import getReactRepositories from "../helpers/getReactRepositories.js";
 
-
-
+// Componente que renderiza mediante useEffect la consulta que trae del helper getReactRepositories
 const Repositories = () => {
+  const [item, setItem] = useState([]); // se inicializa el estado a un array vacio que será el estado inicial
+  const [totalCount, setTotalCount] = useState([]);
 
-    const [item, setItem] = useState([]);
+  // Funcion usada por useEffect que llama a la funcion del helper cada vez que se renderiza componente
+  const updateitem = () => {
+    getReactRepositories().then((newItem) => {
+      setItem(newItem.items);
+      setTotalCount(newItem);
+    });
+  };
 
-    const updateitem = () => {
-        getReactRepositories()
-        .then((newItem) => {
-            setItem(newItem.items);
-        })
-
-    }
-
-    useEffect(() => {
-        updateitem();
-    }, []);
+  // useEffectse ejecutara cada vez que se renderice una sola vez gracias al arreglo vacio que recibe
+  // recibe 2 parametros la arrow function a ejecutar y un arreglo de dependencias en este caso arreglo vacio
+  useEffect(() => {
+    updateitem();
+  }, []);
 
   return (
     <div>
-        { item.map(({ forks, name, stargazers_count, html_url }) => (<ul>
-           <li>Name: { name } - Stars 🌟: { stargazers_count } - Forks: { forks } - URL: { html_url }</li>
-        </ul>
-
+      <h2>Numero total de items: {totalCount.total_count}</h2>
+      <ul>
+        {item.map((ite) => (
+          <li key={ite.id}>
+            Name: {ite.name}, Stars 🌟: {ite.stargazers_count}, 🍴 Forks:{" "}
+            {ite.forks}, URL: {ite.html_url}
+          </li>
         ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Repositories
+export default Repositories;
--- src/helpers/getItem.js
+++
@@ -1,9 +0,0 @@
-const getReactRepositories = async () => {
-    const SEARCH_ENDPOINT = "https://api.github.com/search/repositories?q=react";
-    const res = await fetch(SEARCH_ENDPOINT);
-    const item = await res.json();
-
-    return item;
-}
-
-export default getReactRepositories;
